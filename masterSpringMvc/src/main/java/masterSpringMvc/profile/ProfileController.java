5c917fb4a4cd054cf21a73cefcb0b38beb8a7a94
SlawomirK <slawomir01krupa@gmail.com>
2015-11-05 10:43:47 +0000

    commit without pictures folder ?

--- masterSpringMvc/src/main/java/masterSpringMvc/profile/ProfileController.java
+++ masterSpringMvc/src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -2,6 +2,7 @@ package masterSpringMvc.profile;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -14,30 +15,39 @@ import masterSpringMvc.date.USLocalDateFormatter;
 
 @Controller
 public class ProfileController {
-	
+
 	@ModelAttribute("dateFormat")
-	public String localeFormat(Locale locale){
+	public String localeFormat(Locale locale) {
 		return USLocalDateFormatter.getPattern(locale);
 	}
 
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	public String displayProfile(ProfileForm profileForm){
-		
-		
+	public String displayProfile(ProfileForm profileForm) {
+
 		return "profile/profilePage";
-		
-		
+
 	}
-	
-	
+
 	@RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {   
+	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
 			return "profile/profilePage";
-	    }
-        System.out.println("save ok" + profileForm);
-        return "redirect:/profile";
-    }
-	
-	
+		}
+		System.out.println("save ok" + profileForm);
+		return "redirect:/profile";
+	}
+
+	@RequestMapping(value = "/profile", params = { "addTaste" })
+	public String addRow(ProfileForm profileForm) {
+		profileForm.getTastes().add(null);
+		return "profile/profilePage";
+	}
+
+	@RequestMapping(value = "/profile", params = { "removeTaste" })
+	public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+		Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+		profileForm.getTastes().remove(rowId.intValue());
+		return "profile/profilePage";
+	}
+
 }

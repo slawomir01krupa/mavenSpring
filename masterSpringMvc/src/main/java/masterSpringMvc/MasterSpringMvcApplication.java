45aded86e1f30e3d841f6d088601bf22ea49ffe4
SlawomirK <slawomir01krupa@gmail.com>
2015-11-05 13:11:21 +0000

    wrong commit

--- masterSpringMvc/src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
+++ masterSpringMvc/src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
@@ -2,9 +2,15 @@ package masterSpringMvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import masterSpringMvc.config.PictureUploadProperties;
 
 @SpringBootApplication
-public class MasterSpringMvcApplication {
+//@EnableConfigurationProperties({PictureUploadProperties.class})
+public class MasterSpringMvcApplication extends WebMvcConfigurerAdapter
+{
 
     public static void main(String[] args) {
         SpringApplication.run(MasterSpringMvcApplication.class, args);
